55ede3085f0f8eb01292935ca80b0d71576127e6
Andres Botta <andresbotta@gmail.com>
2014-05-24 15:30:34 -0300

    redes

---
+++ REDES/redes2014-e2/mensajeria.c
@@ -0,0 +1,108 @@
+// Redes de Computadoras - Curso 1er Semestre 2014
+// Tecnologo en Informatica FIng - CETP
+//
+// Entrega 2  - Programacion con Sockets
+// Sistema básico de Mensajería 
+
+// Integrantes:
+//	Detallar aqui, nombre, apellido y documento de todos los integrantes del grupo.
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+#include <sys/socket.h>
+
+#define MAX_LARGO_MENSAJE 255
+#define MAX_LARGO_ARCHIVO 65535
+
+using namespace std;
+
+void manejadorSenhales (int signal)
+// Manejador de las senhales.
+// Aca se debe implementar la accion a realizar cuando se recibe la senhal
+// Deberia haber un manejador de senhales para cada hijo si hacen cosas distintas
+{
+	if (signal == SIGINT)
+	{
+		cout << "\33[46m\33[31m[" << getpid() << "]" << " SIGINT CTRL+C recibido\33[00m\n"; 
+	}
+	if (signal == SIGTERM)
+	{
+		cout << "\33[46m\33[31m[" << getpid() << "]" << " SIGTERM Terminacion de programa\33[00m\n";
+	}
+	if (signal == SIGSEGV)
+	{
+		cout << "\33[46m\33[31m[" << getpid() << "]" << " SIGSEGV violacion de segmento\33[00m\n";
+	}
+	if (signal == SIGCHLD)
+	{
+		cout << "\33[46m\33[31m[" << "]" << " SIGCHLD \33[00m\n";
+	}
+	if (signal == SIGPIPE)
+	{
+		cout << "\33[46m\33[31m[" << getpid() << "]" << " SIGPIPE \33[00m\n";
+	}
+	if (signal == SIGKILL)
+	{
+		cout << "\33[46m\33[31m[" << getpid() << "]" << " SIGKILL \33[00m\n";
+	}
+	if (signal == SIGSEGV)
+	{
+		cout << "\33[46m\33[31m[" << getpid() << "]" << " SIGSEGV violacion de segmento\33[00m\n";
+	}
+	exit(1);
+}
+
+int main(int argc, char * argv[])
+// En argc viene la cantidad de argumentos que se pasan,
+// si se llama solo al programa, el nombre es el argumento 0
+// En nuestro caso:
+//      - argv[0] es el string "mensajeria", puede cambiar si se llama con otro path.
+//      - argv[1] es el puerto de escucha de mensajeria.
+//      - argv[2] es el ip del servidor de autenticacion.
+//      - argv[3] es el puerto del servidor de autenticación.
+{
+
+	if (argc < 4)
+	{
+			cout << "\33[46m\33[31m[ERROR]:" << " Faltan argumentos: port, ipAuth, portAuth.\33[00m\n";
+			exit (1);
+	}
+
+
+	// Estructuras para el manejo de Senhales
+	// Deberia haber un manejador de señales para cada hijo si hacen cosas distintas
+	// *********************************
+	struct sigaction sa;
+	memset (&sa, 0, sizeof (sa));
+	sa.sa_handler = &manejadorSenhales;
+
+	sigaction(SIGINT, &sa, NULL);
+	sigaction(SIGTERM, &sa, NULL);
+	sigaction(SIGPIPE, &sa, NULL);
+	sigaction(SIGSEGV, &sa, NULL);
+	sigaction(SIGKILL, &sa, NULL);
+	signal(SIGALRM, SIG_IGN);
+	// **********************************
+	
+	cout << "\33[34mRedes de Computadoras 2014\33[39m: Sistema de Mensajeria.\nEscuchando en el puerto " << argv[1] << ".\nProceso de pid: " << getpid() << ".\n";
+	
+	// Se debe Bifurcar el Programa
+	// Al iniciar la parte que recibe imprimir lo siguiente descomentandolo
+	//printf("\33[34mRx\33[39m: Iniciada parte que recepciona mensajes. Pid %d\n", getpid());  
+	// Al iniciar la parte que transmite imprimir lo siguiente descomentandolo
+	//printf("\33[34mTx\33[39m: Iniciada parte que transmite mensajes. Pid %d\n", getpid());
+	
+	// Es indistinto si el padre transmite y el hijo recibe, o viceversa, lo que si al ser distintos porcesos, deben tener distinto pid.
+	
+	// Familiarizarse con los comandos de UNIX ps, ps -as, kill, etc.
+	
+}
