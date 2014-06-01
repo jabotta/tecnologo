622c46cc7b8d4adc320f7f99bae3b573c4619a04
dariolevy <levy.dario@gmail.com>
2014-06-01 22:10:10 +0000

    Update mensajeria.c

--- REDES/ob2/mensajeria.c
+++ REDES/ob2/mensajeria.c
@@ -2,7 +2,7 @@
 // Tecnologo en Informatica FIng - CETP
 //
 // Entrega 2  - Programacion con Sockets
-// Sistema b�sico de Mensajer�a 
+// Sistema básico de Mensajería 
 
 // Integrantes:
 //	Detallar aqui, nombre, apellido y documento de todos los integrantes del grupo.
@@ -19,11 +19,26 @@
 #include <string.h>
 #include <iostream>
 #include <sys/socket.h>
+#include <time.h>
 
 #define MAX_LARGO_MENSAJE 255
 #define MAX_LARGO_ARCHIVO 65535
 
+
 using namespace std;
+
+
+void WriteLogFile(const char* szString)
+{
+	time_t t;
+	struct tm * now;
+
+	t = time(NULL);	
+	now = localtime (&t);
+	FILE* pFile = fopen("logFile.txt", "a");
+	fprintf(pFile, "%i/%i/%i %i:%i - %s\n",now->tm_mday, now->tm_mon + 1, now->tm_year + 1900, now->tm_hour, now->tm_min, szString);
+	fclose(pFile);
+ }
 
 void manejadorSenhales (int signal)
 // Manejador de las senhales.
@@ -32,30 +47,37 @@ void manejadorSenhales (int signal)
 {
 	if (signal == SIGINT)
 	{
+        WriteLogFile("SIGINT CTRL+C recibido");
 		cout << "\33[46m\33[31m[" << getpid() << "]" << " SIGINT CTRL+C recibido\33[00m\n"; 
 	}
 	if (signal == SIGTERM)
 	{
+		WriteLogFile("SIGTERM Terminacion de programa");
 		cout << "\33[46m\33[31m[" << getpid() << "]" << " SIGTERM Terminacion de programa\33[00m\n";
 	}
 	if (signal == SIGSEGV)
 	{
+		WriteLogFile("SIGSEGV violacion de segmento");
 		cout << "\33[46m\33[31m[" << getpid() << "]" << " SIGSEGV violacion de segmento\33[00m\n";
 	}
 	if (signal == SIGCHLD)
 	{
+		WriteLogFile("SIGCHLD");
 		cout << "\33[46m\33[31m[" << "]" << " SIGCHLD \33[00m\n";
 	}
 	if (signal == SIGPIPE)
 	{
+		WriteLogFile("SIGPIPE");
 		cout << "\33[46m\33[31m[" << getpid() << "]" << " SIGPIPE \33[00m\n";
 	}
 	if (signal == SIGKILL)
 	{
+		WriteLogFile("SIGKILL");
 		cout << "\33[46m\33[31m[" << getpid() << "]" << " SIGKILL \33[00m\n";
 	}
 	if (signal == SIGSEGV)
 	{
+		WriteLogFile("SIGSEGV violacion de segmento");
 		cout << "\33[46m\33[31m[" << getpid() << "]" << " SIGSEGV violacion de segmento\33[00m\n";
 	}
 	exit(1);
@@ -68,7 +90,7 @@ int main(int argc, char * argv[])
 //      - argv[0] es el string "mensajeria", puede cambiar si se llama con otro path.
 //      - argv[1] es el puerto de escucha de mensajeria.
 //      - argv[2] es el ip del servidor de autenticacion.
-//      - argv[3] es el puerto del servidor de autenticaci�n.
+//      - argv[3] es el puerto del servidor de autenticación.
 {
 
 	if (argc < 4)
@@ -76,24 +98,72 @@ int main(int argc, char * argv[])
 			cout << "\33[46m\33[31m[ERROR]:" << " Faltan argumentos: port, ipAuth, portAuth.\33[00m\n";
 			exit (1);
 	}
-
-
-	// Estructuras para el manejo de Senhales
-	// Deberia haber un manejador de se�ales para cada hijo si hacen cosas distintas
-	// *********************************
-	struct sigaction sa;
-	memset (&sa, 0, sizeof (sa));
-	sa.sa_handler = &manejadorSenhales;
-
-	sigaction(SIGINT, &sa, NULL);
-	sigaction(SIGTERM, &sa, NULL);
-	sigaction(SIGPIPE, &sa, NULL);
-	sigaction(SIGSEGV, &sa, NULL);
-	sigaction(SIGKILL, &sa, NULL);
-	signal(SIGALRM, SIG_IGN);
-	// **********************************
 	
-	cout << "\33[34mRedes de Computadoras 2014\33[39m: Sistema de Mensajeria.\nEscuchando en el puerto " << argv[1] << ".\nProceso de pid: " << getpid() << ".\n";
+	int pid = fork();	
+	if (pid < 0)
+	{
+		WriteLogFile("Error al bifurcar.");
+		printf ("\33[34mSERVIDOR\33[39m: \33[31mError al bifurcar.\33[39m\n");
+		exit (1);
+	}		
+	
+	//lector
+	if (pid == 0)
+	{
+        // Estructuras para el manejo de Senhales
+    	// Deberia haber un manejador de señales para cada hijo si hacen cosas distintas
+    	// *********************************
+    	struct sigaction sa;
+    	memset (&sa, 0, sizeof (sa));
+    	sa.sa_handler = &manejadorSenhales;
+    
+    	sigaction(SIGINT, &sa, NULL);
+    	sigaction(SIGTERM, &sa, NULL);
+    	sigaction(SIGPIPE, &sa, NULL);
+    	sigaction(SIGSEGV, &sa, NULL);
+    	sigaction(SIGKILL, &sa, NULL);
+    	signal(SIGALRM, SIG_IGN);
+    	// **********************************
+		cout << "\33[34mRedes de Computadoras 2014\33[39m: Sistema de Mensajeria.\nEscuchando en el puerto " << argv[1] << ".\nProceso de pid: " << getpid() << ".\n";
+		printf("\33[34mRx\33[39m: Iniciada parte que recepciona mensajes. Pid %d\n", getpid());
+		WriteLogFile("Iniciada parte que recepciona mensajes.");
+		while (true)
+		{
+					
+		}
+	}
+	
+	//escritor
+	if (pid > 0)
+	{
+        // Estructuras para el manejo de Senhales
+    	// Deberia haber un manejador de señales para cada hijo si hacen cosas distintas
+    	// *********************************
+    	struct sigaction sa;
+    	memset (&sa, 0, sizeof (sa));
+    	sa.sa_handler = &manejadorSenhales;
+    
+    	sigaction(SIGINT, &sa, NULL);
+    	sigaction(SIGTERM, &sa, NULL);
+    	sigaction(SIGPIPE, &sa, NULL);
+    	sigaction(SIGSEGV, &sa, NULL);
+    	sigaction(SIGKILL, &sa, NULL);
+    	signal(SIGALRM, SIG_IGN);
+    	// **********************************
+		
+		cout << "\33[34mRedes de Computadoras 2014\33[39m: Sistema de Mensajeria.\nEscuchando en el puerto " << argv[1] << ".\nProceso de pid: " << getpid() << ".\n";
+		printf("\33[34mTx\33[39m: Iniciada parte que transmite mensajes. Pid %d\n", getpid());
+		WriteLogFile("Iniciada parte que transmite mensajes.");
+		while (true)
+		{
+					
+		}
+	}
+
+
+	
+	
+	
 	
 	// Se debe Bifurcar el Programa
 	// Al iniciar la parte que recibe imprimir lo siguiente descomentandolo
