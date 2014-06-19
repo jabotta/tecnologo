2984f4f8cf10d0d69eafb3c8a7bc3f9373da115b
Darius <darius@ubuntu.(none)>
2014-06-18 21:05:36 -0300

    Interface usuario en spa

--- PA/ob5/IUsuarioControlador.h
+++ PA/ob5/IUsuarioControlador.h
@@ -1,7 +1,15 @@
-#ifndef IUSERCONTROLLER_H
-#define IUSERCONTROLLER_H
-class IUsuarioControlador{
-	
-
+#ifndef IUSUARIOCONTROLADOR_H
+#define IUSUARIOCONTROLADOR_H
+#include <iostream.h>
+#include <string.h>
+//#include ""
+class iUsuarioControlador{
+	public:
+		virtual checkeoNickname(String) = 0;
+		virtual ingresarDatosUsuario(DatoUsuario usuario) = 0;
+		virtual guardarUsuario() = 0;
+		virtual DataInformacionUsuario obtenerInformacionUsuario() = 0;
+		virtual IUserDictionary listarUsuarios() = 0;
+		virtual elegirUsuario(String) = 0;
 };
 #endif
