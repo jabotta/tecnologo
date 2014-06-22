a7951adda4415ea44fb0a21d75b1111a5f3a9f35
rodro7 <rodrigo.rossi.trabal@gmail.com>
2014-06-22 15:21:39 -0300

    va pahi

--- PA/ob5/Usuario.h
+++ PA/ob5/Usuario.h
@@ -1,6 +1,37 @@
 #ifndef USUARIO_H
 #define USUARIO_H
+#include "Datetime.h"
+#include <iostream.h>
+#include <string.h>
 class Usuario {
+	private:
+		String nickname;
+		String nombre;
+		String sexo;
+		Datetime fechaNac;
+		Set<Accion> acciones;
+
+	public:
+
+		Usuario();
+		Usuario(Usuario &);
+		Usuario(String,String,String,Datetime);
+
+		~Usuario();
+
+
+		setNickname(String n);
+		setNombre(String n);
+		setSexo(String s);
+		setFechaNac(Datetime t);
+		setAccion(Set<Accion> s);
+
+		Set<Accion> obtenerAcciones();
+		String getNickname();
+		String getNombre();
+		String getSexo();
+		Datetime getFechaNac();
+
 	
 };
 #endif
