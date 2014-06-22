3dbfc58a333386529a3fa7a47ff4a510208ce1a3
Darius <darius@ubuntu.(none)>
2014-06-22 16:06:12 -0300

    DataUsuario

--- PA/ob5/DataUsuario.h
+++ PA/ob5/DataUsuario.h
@@ -1,7 +1,38 @@
-#ifndef DATAUSUARIO_H 
+#ifndef DATAUSUARIO_H
 #define DATAUSUARIO_H
-class DataUsuario{
+#include <string.h>
+#include "Datetime.h"
 
+class DataUsuario{
+	private:
+		string nickname;
+		string nombre
+		string sexo;
+		int edad;
+		Datetime fechaNac;
+
+	public:
+		// Constructores	   
+	   	DataUsuario(); //por defecto
+		DataUsuario(string, string, string, int, DateTime); // por parametro
+		DataUsuario(DataUSuario &); // por copia
+		
+		// Destructor
+		~DataUsuario();
+		 
+		// Selectores
+		int getNickname() const;
+		int getNombre() const;
+		int getSexo() const;
+		int getEdad() const;
+		int getFechaNac() const;
+		
+		// Modificadores
+		void setNickname(string);
+		void setNombre(string);
+		void setSexo(string);
+		void setEdad(int);
+		void setFechaNac(Datetime);
 
 };
 #endif
