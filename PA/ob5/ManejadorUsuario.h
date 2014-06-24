8bd533d324bf5094e42caa81b3100d4590a2654f
rodro7 <rodrigo.rossi.trabal@gmail.com>
2014-06-23 23:33:10 -0300

    s

---
+++ PA/ob5/ManejadorUsuario.h
@@ -0,0 +1,28 @@
+#ifndef MANEJADORUSUARIO_H
+#define MANEJADORUSUARIO_H
+#include "Usuario.h"
+#include <iostream>
+#include <string>
+#include <map>
+#include <list>
+
+using namespace std;
+
+class ManejadorUsuario {
+
+private:
+
+	map<string, Usuario> usuarios;
+	ManejadorUsuario();
+	~ManejadorUsuario();
+
+public:
+
+static ManejadorUsuario& getInstance();
+
+	bool checkeoNickname(string);
+	void guardarUsuario(Usuario);
+	list<Usuario*>ListarUsuarios();
+	Usuario* elegirUsuario(string);
+};
+#endif
