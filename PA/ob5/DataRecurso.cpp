ea23b3239aeff4fc2282895138cb09a452e9e0ff
rodro7 <rodrigo.rossi.trabal@gmail.com>
2014-06-26 01:07:28 -0300

    quedan errores

---
+++ PA/ob5/DataRecurso.cpp
@@ -0,0 +1,51 @@
+#include "DataRecurso.h"
+DataRecurso::DataRecurso(){
+
+}
+DataRecurso::DataRecurso(const DataRecurso& dr){
+
+}
+DataRecurso::~DataRecurso(){
+
+}
+
+//Selectores
+string DataRecurso::getNombre() const {
+	return nombre;
+}
+DataUsuario DataRecurso::getUsuarioCreo() const {
+	return usuarioCreo;
+}
+string DataRecurso::getDescripcion() const {
+	return descripcion;
+}
+DateTime DataRecurso::getFechaUltimoAcceso() const {
+	return fechaUltimoAcceso;
+}
+DateTime DataRecurso::getFechaCreacion() const {
+	return fechaCreacion;
+}
+string DataRecurso::getUbicacion() const {
+	return ubicacion;
+}
+
+//Modificadores
+void DataRecurso::setNombre(string n) {
+	nombre = n;
+}
+void DataRecurso::setUsuarioCreo(DataUsuario u) {
+	usuarioCreo = u;
+}
+void DataRecurso::setDescripcion(string d) {
+	descripcion = d;
+}
+void DataRecurso::setFechaUltimoAcceso(DateTime fua) {
+	fechaUltimoAcceso = fua;
+}
+void DataRecurso::setFechaCreacion(DateTime fc) {
+	fechaCreacion = fc;
+}
+void DataRecurso::setUbicacion(string u){
+	ubicacion = u;
+
+}
---
+++ PA/ob5/Recurso.cpp
@@ -0,0 +1,56 @@
+#include "Recurso.h"
+
+Recurso::Recurso(){
+
+}
+Recurso::Recurso(const Recurso& rCopy){
+
+}
+//Destructor
+Recurso::~Recurso(){
+
+}
+
+void Recurso::Recurso::setNombre(string n ){
+	nombre = n;
+}
+void Recurso::Recurso::setUbicacion(string u){
+	ubicacion = u;
+}
+void Recurso::setUsuarioCrea(Usuario u ){
+	usuarioCrea = u;
+}
+void Recurso::setDescripcion(string d){
+	descripcion = d;
+}
+void Recurso::setFechaUltimoAcceso(DateTime fua){
+	fechaUltimoAcceso = fua;
+}
+void Recurso::setFechaCreacion(DateTime fc){
+	fechaCreacion = fc;
+}
+void Recurso::setFechaModificacion(DateTime fm){
+	fechaModificacion = fm;
+}
+
+string Recurso::getNombre() const{
+	return nombre;
+}
+string Recurso::getUbicacion() const{
+	return ubicacion;
+}
+Usuario Recurso::getUsuarioCrea() const{
+	return usuarioCrea;
+}
+string Recurso::getDescripcion() const{
+	return descripcion;
+}
+DateTime Recurso::getFechaUltimoAcceso() const{
+	return fechaUltimoAcceso;
+}
+DateTime Recurso::getFechaCreacion() const{
+	return fechaCreacion;
+}
+DateTime Recurso::getFechaModificacion() const{
+	return fechaModificacion;
+}
